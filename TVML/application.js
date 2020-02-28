e8b34385fd0fda6c2162ad0e0c18296298a35d7b
gongqingfeng <894237294@qq.com>
2020-02-29 04:50:28 +0800

    add debug

--- TVML/application.js
+++ TVML/application.js
@@ -1630,6 +1630,8 @@ function playBangumi(sid, videoInfo, quality=80)
 
 function openVideoWindow(aid, detail, imageURL, page, title, desc, isBangumi = null)
 {
+        var alert = new tvOS.template.alert('请问要重新加载应用程序吗？', detail, null,'此操作不可逆');
+        alert.presentModal();
 		let videoList = new DMPlaylist();
 		let video = new DMMediaItem('video', detail.durl[0].url);
 		video.url = detail.durl[0].url;
